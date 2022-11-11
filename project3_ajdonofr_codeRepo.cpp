7ab108aac2de535a7dccb39ad6caf9a572dbdc22
ajdonofr <70998110+ajdonofr@users.noreply.github.com>
2022-11-10 21:43:28 -0500

    Add files via upload

---
+++ project3_ajdonofr_codeRepo.cpp
@@ -0,0 +1,23 @@
+#include "mbed.h"
+/*Project 3
+Andrew D'Onofrio
+Created: 11/10/2022
+Purpose: Implement Project 2 for CSE321
+Last updated: 11/10/2022
+*/
+
+// Implementation of algorithm based on keypad presses
+int algorithm()
+{
+    while (true) {
+
+    }
+}
+
+// main() runs in its own thread in the OS
+int main()
+{
+    while (true) {
+
+    }
+}
